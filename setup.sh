db639bbf15e7a81c9a0721688ace642e671fcb1b
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2018-02-17 17:28:41 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+
+nodejs -v
+
+npm install
+
 mkdir ssl
 openssl req -x509 -nodes -days 9999 -newkey rsa:2048 -keyout ssl/gmodremote.key -out ssl/gmodremote.crt
 openssl dhparam -out ssl/dhparam.pem 2048
